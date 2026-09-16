00d771f66100a60ffdd6bb2b6999d98fc889644b
Durga S <durgaselvi8060@gmail.com>
2026-09-16 18:00:35 +0530

    first commit

---
+++ p21.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class p2{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        
+        }
+    }
