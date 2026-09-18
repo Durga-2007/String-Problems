07771921ab5de2649c5f70956c8cf6d426a6ba8b
Durga S <durgaselvi8060@gmail.com>
2026-09-18 18:07:44 +0530

    twenty two commit

---
+++ p22.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class p22 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int maxCount = 0;
+        char ch = ' ';
+        for(int i = 0; i < str.length(); i++) {
+            int count = 0;
+            for(int j = i + 1; j < str.length(); j++) {
+                if(str.charAt(i) == str.charAt(j)) {
+                    count++;
+                }
+            }
+            if(count > maxCount){
+                maxCount = count;
+                ch = str.charAt(i);
+            }
+        }
+        System.out.println(ch);
+        
+    }
+}
